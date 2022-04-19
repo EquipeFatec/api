33a902877ca0631d14c8b64a217c13cf602c0d10
alexiakarine <alexiakarine18@gmail.com>
2022-04-18 22:54:10 -0300

    front exibir valor

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,22 @@
-import { createApp } from 'vue'
+import { createApp, VueElement } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/saga-purple/theme.css'       //theme
 import 'primevue/resources/primevue.min.css'                //core css
 import 'primeicons/primeicons.css'                           //icons
 import router from './router'
+//import store from './store'
+import axios from 'axios'
 import ToastService from 'primevue/toastservice'
+axios.defaults.baseURL = 'http://localhost:8081/'
+
+//vue.config.productionTIP = false
+
+// new VueElement({
+//      router,
+//      store,
+//      render: h =>(App)
+//  }) .$mount('#app')
 
 createApp(App)
     .use(router)
